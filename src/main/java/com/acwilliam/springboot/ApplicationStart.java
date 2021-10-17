70e8fad89f6b5c75915d35d002ff52b4dae10ae6
William Carvalho <williamcoelho.noorden@sulamerica.com.br>
2021-10-17 16:14:11 -0300

    hot swap

--- src/main/java/com/acwilliam/springboot/ApplicationStart.java
+++ src/main/java/com/acwilliam/springboot/ApplicationStart.java
@@ -2,12 +2,14 @@ package com.acwilliam.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-@EnableAutoConfiguration
-@ComponentScan
-@Configuration
+//@EnableAutoConfiguration
+//@ComponentScan
+//@Configuration
+@SpringBootApplication
 public class ApplicationStart {
     public static void main(String[] args) {
         SpringApplication.run(ApplicationStart.class, args);
